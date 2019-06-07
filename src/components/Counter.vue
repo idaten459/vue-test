97fa0023202f21ad4511cf9bcbea753032bd2098
idaten <shukai2013@gmail.com>
2019-06-07 19:28:09 +0900

    counterを作る

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div>{{countMassage}}</div>
+        <button @click="count++">click!</button>
+        <button @click="count = 0">reset!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "counter",
+    data(){
+        return {
+            count: 0
+        };
+    },
+    computed:{
+        countMassage(){
+            return "回数: " + this.count;
+        }
+    }
+};
+</script>
+
+<style></style>
