0cea7d0317df7b13cb902352abaa5016c4901357
idaten <shukai2013@gmail.com>
2019-06-14 22:35:01 +0900

    wakarazu

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,7 +2,20 @@
     <div>
         <div>{{countMassage}}</div>
         <button @click="count++">click!</button>
-        <button @click="count = 0">reset!</button>
+        <button @click="count=0">reset!</button><br>
+        <input type="text" v-model="input1" placeholder="input here">
+        <button @click="add">add</button>
+        <button @click="add2">add2</button>
+        <ul>
+            <li v-for="item in items" :key="item.name">
+                <template v-if="!item.selected">
+                    {{item.name+"po"}}
+                </template>
+                <template v-else>
+                    {{item.name+"hoge"}}
+                </template>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -11,15 +24,46 @@ export default {
     name: "counter",
     data(){
         return {
-            count: 0
+            count: 0,
+            input1 : "",
+            items:[]
         };
     },
     computed:{
         countMassage(){
             return "回数: " + this.count;
         }
+    },
+    methods:{
+        reset : function(){
+            this.count = 0;
+            return ;
+        },
+        add : function(){
+            if(this.input1!=""){
+                console.log(this.input1);
+                this.items.push({name:this.input1},{selected:true})
+                this.input1 = "";
+                return ;
+            }
+        },
+        add2 : function(){
+            if(this.input1!=""){
+                console.log(this.input1);
+                this.items.push({name:this.input1},{selected:false})
+                this.input1 = "";
+                return ;
+            }
+        }
     }
 };
+
 </script>
 
-<style></style>
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+</style>
